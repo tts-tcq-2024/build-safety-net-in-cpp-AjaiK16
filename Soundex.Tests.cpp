9a134d2a7344a9eb6294db63c480d9230e248c8c
AjaiK16 <ajaikannank16@gmail.com>
2024-07-03 13:47:50 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,4 +1,5 @@
-#include <gtest/gtest.h>
+/*
+    #include <gtest/gtest.h>
 #include "Soundex.h"
 
 
@@ -8,5 +9,30 @@ TEST(SoundexTest, HandlesEmptyString) {
 
 TEST(SoundexTest, HandlesSingleCharacter) {
     EXPECT_EQ(generateSoundex("A"), "A000");
+}
+*/
+#include <gtest/gtest.h>
+#include "Soundex.h"
+
+TEST(SoundexTest, HandlesEmptyString) {
+    EXPECT_EQ(generateSoundex(""), "0000");
+}
+
+TEST(SoundexTest, HandlesSingleCharacter) {
+    EXPECT_EQ(generateSoundex("A"), "A000");
+}
+
+TEST(SoundexTest, HandlesStandardNames) {
+    EXPECT_EQ(generateSoundex("Robert"), "R163");
+    EXPECT_EQ(generateSoundex("Rupert"), "R163");
+    EXPECT_EQ(generateSoundex("Rubin"), "R150");
+    EXPECT_EQ(generateSoundex("Ashcraft"), "A261");
+    EXPECT_EQ(generateSoundex("Tymczak"), "T522");
+    EXPECT_EQ(generateSoundex("Pfister"), "P236");
+}
+
+TEST(SoundexTest, HandlesRepetitiveCharacters) {
+    EXPECT_EQ(generateSoundex("BbBb"), "B100");
+    EXPECT_EQ(generateSoundex("Bcdl"), "B234");
 }
 
